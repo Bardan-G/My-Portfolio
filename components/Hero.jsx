63f53c48f53f2778b3b0988587b31c70efce8967
Bardan-g <neupanebardang@gmail.com>
2025-05-19 23:00:58 +0545

    added images and span tags and some styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section id='home' className=''> 
-    <div className=''>
+    <section id='home' className=' pt-28 lg:pt-26 '> 
+       <div className='container lg:grid lg:grid-cols-2 items-center lg:gap-0  '>
         <div>
-            <div>
-                <figure className="">
+            <div className='flex items-center gap-3'>
+                <figure className=" img-box w-9 h-9 rounded-lg">
                     <img src="../src/images/avatar-1.jpg" width={40} height={40} alt="Bardan Portfolio " className='img-cover' />
                 </figure>
-                <div className="">
-                    <span className="">
-                        <span className=""></span>
+                <div className=" flex flex-row items-center  gap-1.5 text-zinc-400 text-sm tracking-wide">
+                    <span className="relative bg-emerald-400 h-2 w-2  rounded-full  ">
+                        <span className=" "></span>
                         Available for work
 
                     </span>
