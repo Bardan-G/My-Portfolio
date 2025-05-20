9426bd93493d962b90180006091e05a5b994d9aa
Bardan-g <neupanebardang@gmail.com>
2025-05-20 15:58:16 +0545

    about section completed using maping label number and key and one variable  styling logos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
     <Header />
     <main>
       <Hero/>
+      <About/>
     </main>
     </>
   )
