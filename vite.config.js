f39d1fd0a1d866504fca2c31e9c0336f71fc2f69
Bardan-g <neupanebardang@gmail.com>
2025-05-22 19:00:22 +0545

    added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({
   base: '/',
   plugins: [tailwindcss() ,react()],
   
