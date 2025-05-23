f82abc49218b6f3be586258c24256567d9aa0ac2
Bardan-g <neupanebardang@gmail.com>
2025-05-23 18:42:04 +0545

    added tailwind css and defineConfi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default  ({
+export default {defineConfig}  ({
   base: '/',
   plugins: [tailwindcss(),react()],
   
